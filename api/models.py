82aad4bba1addff7d4358640e7075ee74a9b859a
Atef Arman Shishir <atef.arman@yahoo.com>
2022-03-19 22:08:59 +0600

    Fourth commit

--- api/models.py
+++ api/models.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False, null=True, blank=True)
-
 
     def __str__(self):
         return self.title
