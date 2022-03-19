62686d4c974926bdc03cc5133c0b40f7c0a1d6f8
Atef Arman Shishir <atef.arman@yahoo.com>
2022-03-19 17:53:49 +0600

    First Commit

---
+++ api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from knox import views as knox_views
+from rest_framework_simplejwt import views as jwt_views
+from . import views
+from .views import RegisterAPI, LoginAPI, getRoutes
+
+urlpatterns = [
+    path('', views.getRoutes),
+    path('get-task/', views.getTask),
+    path('add-task/', views.addTask),
+    path('update-task/<str:pk>', views.updateTask),
+    path('view-task/<str:pk>', views.viewTask),
+    path('delete-task/<str:pk>', views.deleteTask),
+    path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('token/', jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+]
---
+++ api/views.py
@@ -0,0 +1,148 @@
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+
+from rest_framework import generics, permissions, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+from knox.views import LoginView as KnoxLoginView
+from knox.models import AuthToken
+
+import jwt
+import datetime
+
+from .models import Task
+from .serializers import TaskSerializer, UserSerializer, RegisterSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        {'GET': '/get-task'},
+        {'POST': '/add-task'},
+        {'POST': '/register'},
+        {'POST': '/login'},
+        {'POST': '/logout'},
+        {'POST': '/change-password'},
+        {'POST': '/token'},
+        {'POST': '/token/refresh'},
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getTask(request):
+    token = request.META.get('HTTP_AUTHORIZATION')
+    if not token:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    token = token.split(' ')[1]
+
+    payload = jwt.decode(token, 'secret', algorithms='HS256')
+
+    tasks = Task.objects.filter(owner=payload['id'])
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addTask(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response({
+        'success': True,
+        'message': 'Task deleted'
+    })
+
+# update task status
+
+
+@api_view(['POST'])
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "success": True,
+            "task": serializer.data
+        })
+    else:
+        return Response({
+            'success': False,
+            'error': serializer.errors
+        })
+
+
+@api_view(['GET'])
+def viewTask(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task)
+    return Response({
+        "success": True,
+        'task': serializer.data
+    })
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+
+        serializer = self.get_serializer(data=data)
+
+        if User.objects.filter(email=data['email']).exists():
+            return Response({'error': 'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            payload = {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email
+            }
+            token = jwt.encode(payload, 'secret', algorithm='HS256')
+            return Response({
+                'user': token,
+                'success': True
+            })
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow(),
+            'username': user.username,
+            'email': user.email
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        return Response({
+            'user': token,
+            'success': True
+        })
